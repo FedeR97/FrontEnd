3f13bcae506e869cde838867bc2779778abe3370
FedeR97 <fede_210797@hotmail.com>
2022-08-17 19:00:29 -0300

    BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/FAR/FarApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.FAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/FAR/FarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
