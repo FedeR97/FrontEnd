37f8c40ee7ed1006134098dbf24c987de963f981
FedeR97 <fede_210797@hotmail.com>
2022-08-20 02:21:37 -0300

    BackEnd creado

---
+++ BackEnd/src/main/java/com/portfolio/FAR/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.FAR.Repository;
+
+import com.portfolio.FAR.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
