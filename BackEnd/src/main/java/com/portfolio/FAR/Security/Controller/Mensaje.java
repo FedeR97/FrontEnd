0f07c5f2b9e5af48ab9de718c06871a573b83fe5
FedeR97 <fede_210797@hotmail.com>
2022-09-07 02:29:53 -0300

    Modificación del BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/FAR/Security/Controller/Mensaje.java
@@ -0,0 +1,27 @@
+
+package com.portfolio.FAR.Security.Controller;
+
+public class Mensaje {
+    private String mensaje;
+    
+    //construck
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //get and set
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
